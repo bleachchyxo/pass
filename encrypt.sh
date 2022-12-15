22036c1ebccf15c009979105b8eea5df97130361
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2022-12-15 17:04:53 -0600

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
 #encrypt.sh
 
-read -sp 'Insert key:' key # The password for decrypting
+read -sp 'Insert key:' key # Encrypting/Decrypting key
 echo " "
-read -sp 'Insert password:' password # The actual password being ecrypted
+read -sp 'Insert password:' password # Password
 echo " "
 echo Key: $key
 echo Password: $password
 
-# Making the file where the password will be stored
+# file that stores the password
 echo "pass " > foofile
 
-# Compares lenght between keypass and password
+# compares lenght between keypass and password
 if [ ${#key} -lt ${#password} ]; then
         echo "password is bigger"
 fi
 
 for ((i=0; i<${#password}; i++)); do
 
-        keyConvertion="$((2#"$(echo "${key:i:1}" | xxd -b | awk '{print $2}')"))" # ASCII > BINARY > DECIMAL
-        passwordConvertion="$((2#"$(echo "${password:i:1}" | xxd -b | awk '{print $2}')"))" # ASCII > BINARY > DECIMAL
-        addition="$(($keyConvertion+$passwordConvertion))" # Adds the two decimal values
+        keyConvertion="$((2#"$(echo "${key:i:1}" | xxd -b | awk '{print $2}')"))" # ascii > binary > decimal
+        passwordConvertion="$((2#"$(echo "${password:i:1}" | xxd -b | awk '{print $2}')"))" # ascii > binary > decimal
+        addition="$(($keyConvertion+$passwordConvertion))" # adds the two decimal values
+        binaryConvertion="$(echo "obase=2;$addition" | bc)" # decimal > binary
 
-        if [ $addition -lt 255 ]; then
+        if [ ${#binaryConvertion} -lt 8 ]; then
 
-                binaryConvertion="$(echo "obase=2;$addition" | bc)" # Sum result > BINARY
-
-                if [ ${#binaryConvertion} -eq 8 ]; then
-
-                        character="$(echo "$binaryConvertion")"
-                        string="$(echo "character" | perl -lape '$_=pack"(B8)*",@F')" # BINARY > ASCII
-                        sed "s/\$/${string}/" -i foofile
-                        
-                else
-                        #Filling the missing bits
-                        missingBits="$((8-${#binaryConvertion}))"
-                        character="$(echo "$(printf "%0*d" "$missingBits" 0)$binaryConvertion")"
-                        string="$(echo "$character" | perl -lape '$_=pack"(B8)*",@F')" # BINARY > ASCII
-                        sed "s/\$/${string}/" -i foofile
-
-                fi
-
-
+                # filling the missing characters in the octet
+                missingBits="$((8-${#binaryConvertion}))"
+                character="$(echo "$(printf "%0*d" "$missingBits" 0)$binaryConvertion")"
+                string="$(echo "$character" | perl -lape '$_=pack"(B8)*",@F')" # binary > ascii
+                sed "s/\$/${string}/" -i foofile
+        else
+                character="$(echo "$binaryConvertion")"
+                string="$(echo "character" | perl -lape '$_=pack"(B8)*",@F')" # binary > ascii
+                sed "s/\$/${string}/" -i foofile
         fi
-
-
 done
